d859bc6003e16ffd08fe1837092b815b2045f725
mohdareeb <mohdareeb.1224@gmail.com>
2024-07-26 13:05:54 +0530

    react-episode-03

--- App.js
+++ App.js
@@ -5,14 +5,28 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"Im h1"),
-         React.createElement("h2",{},"Im h2")   
-        ]
+// const parent=React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//         [React.createElement("h1",{},"Im h1"),
+//          React.createElement("h2",{},"Im h2")   
+//         ]
+//     )
+// );
+
+const heading = (<h1>Namaste React</h1>)  // this react element 
+
+// functional component
+const Funct = ()=>{
+    return (
+        <div className="container">
+            {heading}
+            <h2>This is second heading</h2>
+            <h3>This is third heading</h3>
+        </div>
     )
-);
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent)
+root.render(<Funct/>)
