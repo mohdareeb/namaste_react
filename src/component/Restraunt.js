e1d0a3441617e29e6af10dfbf46431df1c404f0d
mohdareeb <mohdareeb.1224@gmail.com>
2024-08-05 22:17:55 +0530

    add some more functionality

--- src/component/Restraunt.js
+++ src/component/Restraunt.js
@@ -1,11 +1,13 @@
 import {useEffect,useState} from "react";
 import { useParams } from "react-router-dom";
 import useRestrauntMenu from "./useRestrauntMenu";
+import {CON_URL} from "../utils/constant";
 
 const Restraunt=()=>{
     const {id} = useParams();
     console.log(id)
     const [show,setShow]=useState(false)
+    const [hide,setHide]=useState(false);
     const menu= useRestrauntMenu(id);
     console.log(menu);
     return (
@@ -16,10 +18,10 @@ const Restraunt=()=>{
                 console.log(item);
                 
                 return (
-                <div className="bg-slate-200 mx-auto my-4 p-6  shadow-lg w-56 text-center justify-between" key ={index}>
+                <div className="bg-slate-200 mx-auto my-4 p-6  w-6/12 shadow-lg w-56 text-center justify-between" key ={index}>
                     
                     <span className="flex justify-between">
-                        <h2>{item.title}</h2>
+                        <h2 className="font-bold">{item.title}</h2>
                         <h2 onClick={(e)=>{
                             console.log("clicke");
                             show === false ? setShow(true) : setShow(false)
@@ -30,22 +32,30 @@ const Restraunt=()=>{
                     
                         {
                         
-
+                        
                         show===true ? item?.itemCards?.map((item2,index)=>{
                             
-                            return (
+                            if(item2?.card?.info?.imageId)  return (
                                 <div key ={index}>
-                                <div className="flex justify-between">
+                                <div className="flex justify-between border border-black m-2 p-4">
+                                    <div>
                                     <h2 className="text-left">{item2.card.info.name}</h2>
-                                    <h2> {item2.card.info.price/100}</h2>
+                                    <h2> Rs.{item2.card.info.price/100}</h2>
+                                    
+                                    </div>
+                                    <img src = {CON_URL + item2.card.info.imageId} className="w-40" />
+                                    <div className=" flex justify-between">
+                                        <button className="bg-black text-white h-8 p-2 rounded-lg">Add this </button>
+                                    </div>
                                 </div>
+                                
                                 <hr/>
                                 </div>
                             )
-                        
+                            else null;
                         })
                         :
-                        <h2></h2>
+                        null
                         }
 
                     
